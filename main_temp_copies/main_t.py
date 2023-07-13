46de21bbac067577c9f4803afd36708e50abb220
Kush Joshi <76556985+KushJoshi16@users.noreply.github.com>
2023-07-13 18:24:21 +0530

    first commit

---
+++ main_temp_copies/main_t.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n.pt')  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model(['./assets/im1.jpg', './assets/im2.jpg'])  # return a list of Results objects
+
+# print(results)
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bbox outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Class probabilities for classification outputs
+    print(boxes, masks, keypoints, probs)
